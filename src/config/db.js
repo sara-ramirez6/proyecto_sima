479912bfdc3ec815685682eca50e497cc21ed2bc
sara <saramarcelaramirezbrinez@gmail.com>
2026-09-15 09:01:03 -0500

    Cambios de sara

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql2/promise");
 
 const conexion = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "sima_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
 });
 
 module.exports = conexion;
